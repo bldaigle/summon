f3bce3a7bdc5b8b1c9e5be9dbbf2ed51cc8a0481
bldaigle <bldaigle@owu.edu>
2013-09-17 10:08:36 -0400

    Update summon-tweak.js

--- summon-tweak.js
+++ summon-tweak.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function() {
 	$("#topbar").find(".link").prepend('<a href="http://library.owu.edu/summon.html">Search OhioLINK books</a>');
 
 
-  // Fix the broken saved items bar at the bottom of the page //
+  	// Fix the broken saved items bar at the bottom of the page //
 	
 	if($("#dock-container").length > 0) {
 	 
